748381ee5fd3258f2df9be7599374c2f025fbe36
PetrOFFsky <validol.se@gmail.com>
2022-09-21 12:53:35 +0300

    Create new file

---
+++ main.py
@@ -0,0 +1,16 @@
+x = 48
+print(x % 4 != 0)
+
+a = 5
+b = 6
+print(a > 0 and b < 9)
+
+a = 57
+print(a % 10 == 7)
+
+a = 'Hello'
+print((a + ' ') * 5)
+
+s = input()
+print('Вы ввели', len(s), 'символов')
+print('я' in s)
