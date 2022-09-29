16f422352440dff56d1629969ce7628fac628261
PetrOFFsky <validol.se@gmail.com>
2022-09-29 14:47:31 +0300

    DZ

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-x = 48
-print(x % 4 != 0)
+# x = 48
+# print(x % 4 != 0)
+#
+# a = 5
+# b = 6
+# print(a > 0 and b < 9)
+#
+# a = 57
+# print(a % 10 == 7)
+#
+# a = 'Hello'
+# print((a + ' ') * 5)
+#
+# s = input()
+# print('Вы ввели', len(s), 'символов')
+# print('я' in s)
 
-a = 5
-b = 6
-print(a > 0 and b < 9)
 
-a = 57
-print(a % 10 == 7)
+# i = 1
+# for color in 'red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'violet':
+#     print('#', i, ' color of rainbow is ', color, sep='')
+#     i += 1
 
-a = 'Hello'
-print((a + ' ') * 5)
 
-s = input()
-print('Вы ввели', len(s), 'символов')
-print('я' in s)
+str = 'ABCDEFGHIJK'
+print(str[:])  # весь список
+print(str[::2])  # нечетные по порядку элементы
+print(str[1::2])  # четные по порядку элементы
+print(str[::-1])  # все элементы в обратном порядке
+print(str[5:])  # все элементы начиная с шестого
+print(str[:5])  # все элементы не доходя до шестого
+print(str[-2:1:-1])  # все элементы от предпоследнего до третьего в обратном порядке
+
+
