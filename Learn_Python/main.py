4551a77c80b06335b422568adffad0bf40de791a
PetrOFFsky <validol.se@gmail.com>
2022-10-04 23:49:34 +0300

    part4 in proces 04.10.22

--- Learn_Python/main.py
+++ Learn_Python/main.py
@@ -1,2 +1,25 @@
-for num in range(10, 5, -1):
-    print(num)
+# res = 1
+# for i in range(1, 4):
+#     res *= i
+# print(res)
+#
+# res = 1
+# for i in range(1, 5):
+#     res *= i
+# print(res)
+#
+# res = 1
+# for i in range(1, 6):
+#     res *= i
+# print(res)
+
+
+def factorial(n):
+    res = 1
+    for i in range(1, n + 1):
+        res *= i
+    return res
+
+
+print(factorial(3))
+print(factorial(5))
